dd4b7b526b352a45a2046f3c7835149187390791
Morris Richman <81453549+Mcrich23@users.noreply.github.com>
2024-05-03 19:01:01 -0700

    Renamed NotPrivateApis to NotPrivateAPIs

--- Package.swift
+++ Package.swift
@@ -4,12 +4,12 @@
 import PackageDescription
 
 let package = Package(
-    name: "NotPrivateApis",
+    name: "NotPrivateAPIs",
     products: [
         // Products define the executables and libraries a package produces, making them visible to other packages.
         .library(
-            name: "NotPrivateApis",
-            targets: ["NotPrivateApis"]),
+            name: "NotPrivateAPIs",
+            targets: ["NotPrivateAPIs"]),
     ],
     dependencies: [
         // other dependencies
@@ -20,7 +20,7 @@ let package = Package(
         // Targets are the basic building blocks of a package, defining a module or a test suite.
         // Targets can depend on other targets in this package and products from dependencies.
         .target(
-            name: "NotPrivateApis",
+            name: "NotPrivateAPIs",
             dependencies: [
                 .product(name: "Markdown", package: "swift-markdown"),
             ]
