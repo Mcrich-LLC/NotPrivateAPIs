cbb810c7b6b1fa2979162b31445aadca46896bb3
Morris Richman <81453549+Mcrich23@users.noreply.github.com>
2024-05-04 17:57:17 -0700

    Added main sources for the rest of the site

---
+++ build-site.sh
@@ -0,0 +1,21 @@
+#! /bin/bash
+
+if [ -d "./Build" ]; then
+    rm -rf ./Build
+fi
+
+if [ -d "./DocBuild" ]; then
+    rm -rf ./DocBuild
+fi
+
+mkdir ./Build
+
+cp -r './Main Site Sources/' ./Build/ && swift package --allow-writing-to-directory ./ \
+    generate-documentation --target NotPrivateAPIs \
+    --disable-indexing \
+    --transform-for-static-hosting \
+    --experimental-enable-custom-templates \
+    --hosting-base-path / \
+    --output-path ./DocBuild \
+
+cp -nr ./DocBuild/. ./Build/ || true
